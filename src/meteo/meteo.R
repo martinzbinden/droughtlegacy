219e63377008d43c741e38d76e1c8b1d102bb65f
Martin Zbinden <martin.zbinden@gmail.com>
2014-10-14 20:33:10 +0200

    added data, renewed R-files

--- src/meteo/meteo.R
+++ src/meteo/meteo.R
@@ -1,20 +1,23 @@
 # automatic dataimport
 
+getwd()
+
 #Binn
 # Beispiel: BIN <- "http://www.meteoschweiz.admin.ch/files/metweb/verlaufsgrafiken/TagVerlaufRegen.BIN.gif"
 stations <- c("BER", "THU", "VIS", "BRI", "BIN", "ULR", "COM", "SBO")
 baseurl <- "http://www.meteoschweiz.admin.ch/files/metweb/verlaufsgrafiken/TagVerlaufRegen"
-destfile.folder <- "meteodata/"
+destfile.folder <- "data/meteo/gif/"
 destfile.suffix <- "gif"
 
 getrain(stations, baseurl, destfile.folder, destfile.suffix)
 
 getrain <- function(stations, baseurl, destfile.folder, destfile.suffix) {
   result <- ""
+  
   for (station in stations) {
     #station <- "BER"
     url <- paste(baseurl, station, destfile.suffix,sep=".")
-    destfile.name <- paste(destfile.folder,station,Sys.Date(),sep="")
+    destfile.name <- paste(destfile.folder,Sys.Date(),"/",station,Sys.Date(),sep="")
     destfile <- paste(destfile.name,destfile.suffix,sep=".")
     download.file(url, destfile,quiet=F)
     result <- append(result, destfile)
@@ -22,7 +25,3 @@ getrain <- function(stations, baseurl, destfile.folder, destfile.suffix) {
   return(destfile)
 }
 
-
-download.file(url, destfile, method, quiet = FALSE, mode = "w",
-              cacheOK = TRUE,
-              extra = getOption("download.file.extra"))

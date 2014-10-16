840bcacaa6f9b66f9a807d1dd58e70c9ade3be84
Martin Zbinden <martin.zbinden@gmail.com>
2014-10-16 21:07:51 +0200

    new script to analyze IDAWEB data

--- src/meteo/meteo.R
+++ src/meteo/meteo.R
@@ -1,3 +1,12 @@
 # automatic dataimport
 
 getwd()
+
+df <- read.csv("data/meteo/csv/order_30666_data.txt",header=TRUE,row.names=NULL)
+df <- read.csv("data/meteo/csv/order_30666_data.txt",header=FALSE,row.names=NULL)
+
+
+colnames(df) <- c(colnames(df)[-1],"x")
+
+
+head(df)
